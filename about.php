0164b46a922795599c6497da9a28aff3ddad36d8
DILEEP KUSHWAHA <71811440+dileep8090@users.noreply.github.com>
2022-02-08 23:23:45 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Mix music | about</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style.css">
+</head>
+ 
+<body>
+    
+    <section class="section about-section gray-bg" id="about">
+        <div class="container">
+            <div class="row align-items-center flex-row-reverse">
+                <div class="col-lg-6">
+                    <div class="about-text go-to">
+                        <h3 class="dark-color">About Me</h3>
+                        <h6 class="theme-color lead"> 
+                        <p>I am  curently B-Tech Student in Vits college Satna Mp. i am larning webdevelopment. this website making main motive  inhance  my knowledge  .then i decide making this website
+                         Nowadays I'm working on <b>IDEALVILLAGE</b> as the Software Engineer (SDE). I also like to develop WebApplications. It is my 3rd year in B-tech Computer Science. I love to do competitive Coding and ML projects.</p>
+                        <div class="row about-list">
+                            <div class="col-md-6">
+                                <div class="media">
+                                    <label>Birthday</label>
+                                    <p>6th octber 2002</p>
+                                </div>
+                                <div class="media">
+                                    <label>Age</label>
+                                    <p>19 Yr</p>
+                                </div>
+                                <div class="media">
+                                    <label>Residence</label>
+                                    <p>india</p>
+                                </div>
+                                <div class="media">
+                                    <label>Address</label>
+                                    <p>485446 <br>Nagod Satna mp </p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="media">
+                                    <label>E-mail</label>
+                                    <p>Dileepkushwaha8090@gmail.com</p>
+                                </div>
+                                <div class="media">
+                                    <label>Phone</label>
+                                    <p>8357892218</p>
+                                </div>
+                                <div class="media">
+                                    <label>Skype</label>
+                                    <p>Dileepkushwaha8090@gmail.com</p>
+                                </div>
+                                <div class="media">
+                                    <label>Freelance</label>
+                                    <p>Available</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-avatar">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" title="" alt="">
+                    </div>
+                </div>
+            </div>
+             
+            </div>
+        </div>
+    </section>
+    
+</body>
+ 
+</html>
---
+++ navbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Mix music | Your favourite music is here</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li class="brand"><img src="logo.png" alt="Spotify"> Spotify</li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
